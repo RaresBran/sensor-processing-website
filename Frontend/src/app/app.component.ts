7ab44a71b03dbb680dc4396db15f32d3a0efa945
Rares <rares.bran@yahoo.ro>
2024-06-07 01:52:23 +0300

    Update angular

--- Frontend/src/app/app.component.ts
+++ Frontend/src/app/app.component.ts
@@ -1,6 +1,6 @@
 import {Component} from '@angular/core';
 import {RouterModule, RouterOutlet} from '@angular/router';
-import {HttpClientModule} from "@angular/common/http";
+import {} from "@angular/common/http";
 import {FormsModule} from "@angular/forms";
 import {NavbarComponent} from "./shared/navbar/navbar.component";
 
@@ -11,7 +11,11 @@ import {NavbarComponent} from "./shared/navbar/navbar.component";
     FormsModule,
     RouterOutlet,
     RouterModule,
-    HttpClientModule,
+    
+// TODO: `HttpClientModule` should not be imported into a component directly.
+// Please refactor the code to add `provideHttpClient()` call to the provider list in the
+// application bootstrap logic and remove the `HttpClientModule` import from this component.
+HttpClientModule,
     NavbarComponent,
   ],
   templateUrl: './app.component.html',
